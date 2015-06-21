abc7b1c35c192301fe93a37194580dbd3e4c51ce
hecnabae <hecnabae@gmail.com>
2015-06-21 16:30:29 +0200

    ug

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,6 @@ router.get('/', function (req, res, next) {
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
-router.get('(quizes/author', quizController.authors);
+router.get('/quizes/author', quizController.authors);
 
 module.exports = router;
