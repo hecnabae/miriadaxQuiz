bf5f170409601a5bb8b6dd97af61bcd58acbd9f4
hecnabae <hecnabae@gmail.com>
2015-06-23 09:14:34 +0200

    identificación de versión

---
+++ models/models.js
@@ -0,0 +1,47 @@
+var path = require('path');
+
+// Postgres DATABASE_URL = postgres://user.passwd@host:port/database
+// SQLITE DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6]||null);
+var user = (url[2]||null);
+var pwd = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect = (url[1]||null);
+var port = (url[5]||null);
+var host = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
+
+// Cargar Modelo ORM
+var sequelize = require('sequelize');
+
+// Usar BBDD SQLite:
+var db = new sequelize(DB_name, user, pwd, {
+	dialect: protocol,
+	protocol: protocol,
+	port: port,
+	host: host,
+	storage: storage,
+	omitNull: true
+});
+
+// Importar la definición de la tabla Quiz en quiz.js
+var quiz_path = path.join(__dirname, 'quiz');
+var Quiz = db.import(quiz_path);
+
+exports.Quiz = Quiz; // exportar definición de la tabla Quiz
+
+// sequelize.sync() crea e inicializa tabla de preguntas en DB
+db.sync().success(function () {
+	// success(..) ejecuta el manejador una vez creada la tabla
+	Quiz.count().success(function (count) {
+		if (count === 0) { // la tabla se inicializa solo si está vacía
+			Quiz.create({
+				pregunta: 'Capital de Italia',
+				respuesta: 'Roma'
+			}).success(function () {
+				console.log('Base de datos inicializada')
+			});
+		};
+	});
+});
