8c847fb3db27f65f82ad7e39c7c7e6c3f92854eb
hecnabae <hecnabae@gmail.com>
2015-06-23 10:19:48 +0200

    final

--- controllers/quiz-controller.js
+++ controllers/quiz-controller.js
@@ -11,6 +11,35 @@ exports.question = function (req, res) {
 	})
 };
 
+exports.load = function (req, res, next, quizId) {
+	models.Quiz.find(quizId).then(function (quiz) {
+		if (quiz) {
+			req.quiz = quiz;
+			next();
+		} else {
+			next(new Error('No existe quizid=' + quizId));
+		}
+	}).catch(function8error) {
+		next(error);
+	});
+};
+
+exports.index = function (req, res) {
+	models.Quiz.findAll().then(function (quizes) {
+		res.render('quizes/index.ejs', {
+			quizes: quizes
+		}).catch(function (error) {
+			next(error);
+		});
+	})
+};
+
+exports.show = function (req, res) {
+	res.render('quizes/show', {
+		quiz: req.quiz
+	});
+};
+
 exports.answer = function (req, res) {
 	//	if (req.query.respuesta === 'Roma') {
 	//		res.render('quizes/answer', {
@@ -21,17 +50,27 @@ exports.answer = function (req, res) {
 	//			respuesta: 'Incorrecto'
 	//		});
 	//	}
-	models.Quiz.findAll().success(function (quiz) {
-		if (req.query.respuesta === quiz[0].respuesta) {
-			res.render('quizes/answer', {
-				respuesta: 'Correcto'
-			});
-		} else {
-			res.render('quizes/answer', {
-				respuesta: 'Incorrecto'
-			});
-		}
-	})
+
+
+	//	models.Quiz.findAll().success(function (quiz) {
+	//		if (req.query.respuesta === quiz[0].respuesta) {
+	//			res.render('quizes/answer', {
+	//				respuesta: 'Correcto'
+	//			});
+	//		} else {
+	//			res.render('quizes/answer', {
+	//				respuesta: 'Incorrecto'
+	//			});
+	//		}
+	//	})
+	var resultado = 'Incorrecto';
+	if (req.query.respuesta === req.quiz.respuesta) {
+		resultado = 'Correcto';
+	}
+	res.render('quizes/answer', {
+		quiz: req.quiz,
+		respuesta: resultado
+	});
 };
 
 exports.authors = function (req, res) {
--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,14 @@ router.get('/', function (req, res, next) {
 	});
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
-router.get('/quizes/author', quizController.authors);
+//router.get('/quizes/question', quizController.question);
+//router.get('/quizes/answer', quizController.answer);
+//router.get('/quizes/author', quizController.authors);
+
+router.param('quizId', quizController.load); // autoload :quizId
+
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
