dbdad2824c24fdb96bcdf68558a203891c19ae31
hecnabae <hecnabae@gmail.com>
2015-06-24 22:48:28 +0200

    bug

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(methodOverride('_method'));
+//app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
