d4a9768fd9ea68b15116bba7a05f8efb217cb038
hecnabae <hecnabae@gmail.com>
2015-06-25 20:43:50 +0200

    problemas

--- controllers/quiz-controller.js
+++ controllers/quiz-controller.js
@@ -85,21 +85,21 @@ exports.new = function (req, res) {
 exports.create = function (req, res) {
 	var quiz = models.Quiz.build(req.body.quiz);
 
-	quiz.validate().then(function (err) {
-		if (err) {
-			res.render('quizes/new', {
-				quiz: quiz,
-				errors: err.errors,
-				temas: listaTemas
-			});
-		} else {
+//	quiz.validate().then(function (err) {
+//		if (err) {
+//			res.render('quizes/new', {
+//				quiz: quiz,
+//				errors: err.errors,
+//				temas: listaTemas
+//			});
+//		} else {
 			//Guarda en DB los campos pregunta y respuesta de quiz
 			quiz.save({
 				fields: ["pregunta", "respuesta"]
 			}).then(function () {
 				res.redirect('/quizes')
 			})
-		}
+		//}
 	});
 };
 
