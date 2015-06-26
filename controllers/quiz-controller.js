7676bbb6022af09762600769830cd78b2bb5e42d
hecnabae <hecnabae@gmail.com>
2015-06-26 18:04:04 +0200

    prueba

--- controllers/quiz-controller.js
+++ controllers/quiz-controller.js
@@ -86,24 +86,22 @@ exports.new = function (req, res) {
 exports.create = function (req, res) {
 	var quiz = models.Quiz.build(req.body.quiz);
 
-	res.send(quiz.pregunta + quiz.respuesta + quiz.indice);
-
-//	quiz.validate().then(function (err) {
-//		if (err) {
-//			res.render('quizes/new', {
-//				quiz: quiz,
-//				errors: err.errors,
-//				indices: listaIndices
-//			});
-//		} else {
-//			//Guarda en DB los campos pregunta y respuesta de quiz
-//			quiz.save({
-//				fields: ["pregunta", "respuesta"]
-//			}).then(function () {
-//				res.redirect('/quizes')
-//			})
-//		}
-//	});
+	quiz.validate().then(function (err) {
+		if (err) {
+			res.render('quizes/new', {
+				quiz: quiz,
+				errors: err.errors,
+				indices: listaIndices
+			});
+		} else {
+			//Guarda en DB los campos pregunta y respuesta de quiz
+			quiz.save({
+				fields: ["pregunta", "respuesta", "indice"]
+			}).then(function () {
+				res.redirect('/quizes')
+			})
+		}
+	});
 };
 
 exports.edit = function (req, res) {
