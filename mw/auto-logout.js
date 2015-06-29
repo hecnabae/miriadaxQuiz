003ecbfc226d5320fd4ac1179eed81318deb9afd
hecnabae <hecnabae@gmail.com>
2015-06-29 10:33:52 +0200

    bug

--- mw/auto-logout.js
+++ mw/auto-logout.js
@@ -1,25 +1,20 @@
-var inactTime = 2 * 60 * 1000; // tiempo por defecto en ms
+var inactTime = 2 * 60 * 1000; // 2 min. en ms.
 
 module.exports = function(t) {
 
-	//temporizador
   var temporizador = t || inactTime;
 
   return function(req, res, next) {
     var ahora = new Date().getTime();
 
-    // Obtenemos la ultima hora
     var lastReqTime = req.session.reqTime || ahora;
 
     if (req.session.user && (ahora - lastReqTime > temporizador)) {
       delete req.session.user; // Destruimos la sesión
-      console.log("Inactividad");
     }
 
-    // Actualizamos hora
     req.session.reqTime = ahora;
 
-    // siguiente petición
     next();
   };
 };
